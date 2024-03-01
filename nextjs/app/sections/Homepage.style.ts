f66438fff07c6a93e41ff445e586389be3d806b0
Roman Dietenmeier <roman@d-robotic.de>
2024-03-01 14:22:21 +0100

    Add HomepageArrow link with no functionality.

--- nextjs/app/sections/Homepage.style.ts
+++ nextjs/app/sections/Homepage.style.ts
@@ -17,3 +17,31 @@ export const HomepageHeading = styled.h1`
   font-weight: 900;
   letter-spacing: 0.625rem;
 `;
+
+export const HomepageArrowLink = styled.a`
+  position: absolute;
+  bottom: 1rem;
+`;
+
+export const HomepageArrow = styled.img`
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  animation: scrollDown 2s infinite;
+  animation-delay: 2500ms;
+  @keyframes scrollDown {
+    0%,
+    20%,
+    50%,
+    80%,
+    100% {
+      transform: translateY(0);
+    }
+    40% {
+      transform: translateY(-30px);
+    }
+    60% {
+      transform: translateY(-15px);
+    }
+  }
+`;
--- nextjs/app/sections/Homepage.tsx
+++ nextjs/app/sections/Homepage.tsx
@@ -1,9 +1,17 @@
-import { HomepageHeading, HomepageWrapper } from "./Homepage.style";
+import {
+  HomepageArrow,
+  HomepageArrowLink,
+  HomepageHeading,
+  HomepageWrapper,
+} from "./Homepage.style";
 
 export function Homepage() {
   return (
     <HomepageWrapper>
       <HomepageHeading>Data Space</HomepageHeading>
+      <HomepageArrowLink href="#about">
+        <HomepageArrow src="/arrow-down.svg" />
+      </HomepageArrowLink>
     </HomepageWrapper>
   );
 }

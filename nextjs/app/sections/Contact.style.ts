84840a51f35b3e2ec425e5aa8bfbdf3d470edd17
Roman Dietenmeier <roman@d-robotic.de>
2024-03-02 18:18:22 +0100

    Add initial Contact section content.

---
+++ nextjs/app/sections/Contact.style.ts
@@ -0,0 +1,11 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ContactEmail = styled.a`
+  font-size: 1.5rem;
+`;
+
+export const ContactNumber = styled.p`
+  color: rgba(0, 0, 0, 0.7);
+`;

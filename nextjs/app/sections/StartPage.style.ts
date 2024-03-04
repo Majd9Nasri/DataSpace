d2712674e18713f63336ee7e3ed2f6fc8f3e7b9c
Roman Dietenmeier <roman@d-robotic.de>
2024-03-04 01:21:05 +0100

    Fix StarSection Background Image now doing it like Majd did it is Genius.

--- nextjs/app/sections/StartPage.style.ts
+++ nextjs/app/sections/StartPage.style.ts
@@ -3,30 +3,34 @@ import styled from "styled-components";
 import { ZIndex } from "../../ui/zIndex";
 
 export const StartPageWrapper = styled.section`
+  position: relative;
+
   width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   box-shadow: 0 0 0.625rem #000;
-`;
 
-export const StartPageBackgroundImage = styled.img`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  min-height: 100%;
-  max-height: 100%;
-  width: 100%;
-  min-width: 100%;
-  max-width: 100%;
+  background-image: url("/kam.webp");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 
-  object-fit: cover;
+  z-index: ${ZIndex.Default};
 
-  filter: brightness(30%);
-  z-index: ${ZIndex.StartPageBackgroundImage};
+  &:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #000;
+    opacity: 0.7;
+
+    z-index: ${ZIndex.StartPageBackgroundImage};
+  }
 `;
 
 export const StartPageHeading = styled.h1`
--- nextjs/app/sections/StartPage.tsx
+++ nextjs/app/sections/StartPage.tsx
@@ -2,7 +2,6 @@ import { handleNavLinkClick } from "../../lib/clientUtility";
 import {
   StartPageArrow,
   StartPageArrowLink,
-  StartPageBackgroundImage,
   StartPageHeading,
   StartPageWrapper,
 } from "./StartPage.style";
@@ -10,7 +9,6 @@ import {
 export function StartPage() {
   return (
     <StartPageWrapper id="start">
-      <StartPageBackgroundImage src="/kam.webp" alt="Start page background" />
       <StartPageHeading>Data Space</StartPageHeading>
       <StartPageArrowLink href="#about" onClick={handleNavLinkClick}>
         <StartPageArrow src="/arrow-down.svg" />

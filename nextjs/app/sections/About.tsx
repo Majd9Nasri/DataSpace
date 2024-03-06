41e54998086a805dd0553e997036aac88f584b81
Roman Dietenmeier <roman@d-robotic.de>
2024-03-06 15:28:11 +0100

    Add translation for About Page.

--- nextjs/app/sections/About.tsx
+++ nextjs/app/sections/About.tsx
@@ -1,23 +1,22 @@
+import { useContext } from "react";
 import { ContentSection } from "../../ui/general.style";
+import LanguageContext from "../components/LanguageContext";
 import { NavbarRouteLinks, NavbarRoutes } from "../header/navbarRoutes";
 import { SectionTitle } from "./SectionTitle";
 
 export function About() {
+  const { dictionary } = useContext(LanguageContext);
   return (
     <ContentSection
       id={NavbarRoutes.About}
       className="animate-slide-in-from-bottom-5rem-target-class"
     >
-      <SectionTitle title="About" targetId={NavbarRouteLinks.About} />
-      <h1>
-        We are an interdisciplinary Team of Data Scientists with backgrounds in
-        Economics, Software Engineering and Mechatronics
-      </h1>
-      <p>
-        We are passionate to push the boundaries of what is possible with data
-        and technology, and are committed to continue to innovate and create
-        value based on our vast domain knowledge and expertise for our clients.
-      </p>
+      <SectionTitle
+        title={dictionary.Navbar.About}
+        targetId={NavbarRouteLinks.About}
+      />
+      <h1>{dictionary.About.h1}</h1>
+      <p>{dictionary.About.p}</p>
     </ContentSection>
   );
 }

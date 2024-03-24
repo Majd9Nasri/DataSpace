2d002263e6ac46abdee89f6ab49b1e4d8e607c94
Roman Dietenmeier <roman@d-robotic.de>
2024-03-24 17:20:03 +0100

    Add margin to Team members in Teams section.

--- nextjs/app/sections/Team.style.ts
+++ nextjs/app/sections/Team.style.ts
@@ -1,11 +1,13 @@
 "use client";
 import styled from "styled-components";
+import { ResponsiveFlexGrid } from "../../ui/general.style";
 
 export const TeamImageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 0 0.25rem;
+  margin-bottom: 1rem;
 `;
 
 export const TeamImage = styled.img`
@@ -13,4 +15,10 @@ export const TeamImage = styled.img`
   height: 18.75rem;
   object-fit: cover;
   border-radius: 50%;
+`;
+
+export const TeamResponsiveFlexGrid = styled(ResponsiveFlexGrid)`
+  ${TeamImageWrapper}:last-child {
+    margin-bottom: 0;
+  }
 `;
--- nextjs/app/sections/Team.tsx
+++ nextjs/app/sections/Team.tsx
@@ -3,12 +3,15 @@ import {
   ContentSection,
   ContentSectionHeading,
   ContentSectionWrapper,
-  ResponsiveFlexGrid,
 } from "../../ui/general.style";
 import LanguageContext from "../components/LanguageContext";
 import { NavbarRouteLinks, NavbarRoutes } from "../header/navbarRoutes";
 import { SectionTitle } from "./SectionTitle";
-import { TeamImage, TeamImageWrapper } from "./Team.style";
+import {
+  TeamImage,
+  TeamImageWrapper,
+  TeamResponsiveFlexGrid,
+} from "./Team.style";
 
 export function Team() {
   const { dictionary } = useContext(LanguageContext);
@@ -24,7 +27,7 @@ export function Team() {
         <ContentSectionHeading className="animate-slide-in-from-bottom-5rem-target-class">
           {dictionary.Team.h1}
         </ContentSectionHeading>
-        <ResponsiveFlexGrid columns={3}>
+        <TeamResponsiveFlexGrid columns={3}>
           <TeamImageWrapper className="animate-slide-in-from-bottom-5rem-target-class">
             <TeamImage src="/Ahmed.jpg" alt="Ahmed" />
             <h3>Ahmed Al-Ammari</h3>
@@ -48,7 +51,7 @@ export function Team() {
             <h3>Daniel Pantel</h3>
             <p>M.Sc. in Data Science - B.Sc. in Business Information Systems</p>
           </TeamImageWrapper>
-        </ResponsiveFlexGrid>
+        </TeamResponsiveFlexGrid>
       </ContentSection>
     </ContentSectionWrapper>
   );
